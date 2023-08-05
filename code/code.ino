2192b00b67c27ab85d094b3fd98af34e5bfec933
o2sa <o2sama1@outlook.sa>
2023-08-05 23:50:21 +0300

    add admin page  and some features

--- code/code.ino
+++ code/code.ino
@@ -19,6 +19,11 @@
 #define USE_INTRANET
 #define AP_SSID "Parking-System"
 #define AP_PASS "pass12345"
+
+#include <Preferences.h>
+
+Preferences preferences;
+
 // variable for the IP reported when you connect to your homes intranet (during debug mode)
 IPAddress Actual_IP;
 // definitions of your desired intranet created by the ESP32
@@ -45,7 +50,7 @@ int entry_servo_pos = 0;
 int exit_servo_pos = 0;
 
 // time variables
-int moveTime = 10;
+int moveTime = 15;
 unsigned long int entry_open_time = 0;
 unsigned long int entry_moving_time = 0;
 unsigned long int exit_open_time = 0;
@@ -64,6 +69,10 @@ char buf[32];
 WebServer server(80);
 
 void setup() {
+
+  preferences.begin("variables", false);
+
+  cars_num = preferences.getInt("cars_num", 0);
   // Allow allocation of all timers
   ESP32PWM::allocateTimer(0);
   ESP32PWM::allocateTimer(1);
@@ -115,40 +124,48 @@ void setup() {
   // same notion for the following .on calls
   // add as many as you need to process incoming strings from your web page
   // as you can imagine you will need to code some javascript in your web page to send such strings
-  server.on("/UPDATE_SLIDER", UpdateSlider);
   server.on("/BUTTON_0", ProcessButton_0);
   server.on("/BUTTON_1", ProcessButton_1);
+  server.on("/BUTTON_2", ProcessButton_2);
+  server.on("/BUTTON_3", ProcessButton_3);
+  server.on("/restart", SystemRestart);
+
+
   // finally begin the server
   server.begin();
+
+//  entry_gate_servo.write(0);
+//  exit_gate_servo.write(0);
+
+
+
 }
 
 void loop() {
-
   if (system_status)
   {
-
     //Serial.println("Reading Sensors");
     SensorUpdate = millis();
-    exit_gate_distance = get_distance(ULTRA_TWO_TRIG_PIN, ULTRA_TWO_ECHO_PIN);
     entry_gate_distance = get_distance(ULTRA_ONE_TRIG_PIN, ULTRA_ONE_ECHO_PIN);
 
     if (entry_gate_distance <= 10 && cars_num < 10 && !entry_open) {
       cars_num++;
+      preferences.putInt("cars_num", cars_num);
       entry_open_time = millis();
       entry_moving_time = millis();
       entry_open = true;
     }
 
+    exit_gate_distance = get_distance(ULTRA_TWO_TRIG_PIN, ULTRA_TWO_ECHO_PIN);
     if (exit_gate_distance <= 10 && cars_num >= 1 && !exit_open) {
       cars_num--;
+      preferences.putInt("cars_num", cars_num);
       exit_open_time = millis();
       exit_moving_time = millis();
       exit_open = true;
     }
 
-    Serial.print("Cars Number: ");
-    Serial.println(cars_num);
-
+    
 
 
     //Entry Gate Moving control
@@ -157,7 +174,7 @@ void loop() {
         //      && (entry_gate_distance <= 10)
         && entry_servo_pos < 90) {
       if (millis() - entry_moving_time >= moveTime) {
-        entry_servo_pos = entry_servo_pos +  3;
+        entry_servo_pos = entry_servo_pos +  2;
         entry_gate_servo.write(entry_servo_pos);
       }
       entry_moving_time = millis();
@@ -167,19 +184,26 @@ void loop() {
       if (!(entry_gate_distance <= 10) && entry_servo_pos > 0) {
         entry_open = false;
         if (millis() - entry_moving_time >= moveTime) {
-          entry_servo_pos = entry_servo_pos - 3;
+          entry_servo_pos = entry_servo_pos - 2;
           entry_gate_servo.write(entry_servo_pos);
         }
         entry_moving_time = millis();
       }
     }
+
+    Serial.print("entry_open: ");
+        Serial.println(entry_open);
+        Serial.print("exit_open: ");
+        Serial.println(entry_open);
+
+
     //Exit Gate Moving control
     //check if the gate in open state to open it gradully
     if (exit_open
         //      && (entry_gate_distance <= 10)
         && exit_servo_pos < 90) {
       if (millis() - exit_moving_time >= moveTime) {
-        exit_servo_pos = exit_servo_pos +  3;
+        exit_servo_pos = exit_servo_pos +  2;
         exit_gate_servo.write(exit_servo_pos);
       }
       exit_moving_time = millis();
@@ -189,7 +213,7 @@ void loop() {
       if (!(exit_gate_distance <= 10) && exit_servo_pos > 0) {
         exit_open = false;
         if (millis() - entry_moving_time >= moveTime) {
-          exit_servo_pos = exit_servo_pos - 3;
+          exit_servo_pos = exit_servo_pos - 2;
           exit_gate_servo.write(exit_servo_pos);
         }
         exit_moving_time = millis();
@@ -199,30 +223,24 @@ void loop() {
   server.handleClient();
 }
 
-// function managed by an .on method to handle slider actions on the web page
-// this example will get the passed string called VALUE and conver to a pwm value
-// and control the fan speed
 
 
-void UpdateSlider() {
+void SystemRestart() {
 
-  String t_state = server.arg("VALUE");
 
-  //  // conver the string sent from the web page to an int
-  //  servoPosi = t_state.toInt();
-  //  Serial.print("UpdateSlider"); Serial.println(servoPosi);
-  //  // now set the PWM duty cycle
-  //  myservo.write(servoPosi);
+  if (entry_servo_pos > 0 || exit_servo_pos > 0 ) {
 
-  // now send it back
-  strcpy(buf, "");
-  sprintf(buf, "%d", 20);
-  sprintf(buf, buf);
-  server.send(200, "text/plain", buf); //Send web page
 
+    server.send(200, "text/plain", "false"); //Send web page
+
+  }
+  else {
+    server.send(200, "text/plain", "true"); //Send web page
+    delay(2000);
+    ESP.restart();
+
+  }
 }
-
-
 // now process button_0 press from the web site. Typical applications are the used on the web client can
 // turn on / off a light, a fan, disable something etc
 void ProcessButton_0() {
@@ -237,13 +255,13 @@ void ProcessButton_0() {
 
     }
   }
-  else{
+  else {
     system_status = true;
-      status_led = true;
-      server.send(200, "text/plain", "on"); //Send web page
+    status_led = true;
+    server.send(200, "text/plain", "on"); //Send web page
   }
   digitalWrite(PIN_LED, status_led);
-  
+
 
 }
 // same notion for processing button_1
@@ -255,6 +273,8 @@ void ProcessButton_1() {
 
   if (!entry_open && !exit_open) {
     cars_num = 0;
+    preferences.putInt("cars_num", cars_num);
+
     Serial.println("Cars number reset.");
     server.send(200, "text/plain", "true"); //Send web page
 
@@ -265,7 +285,41 @@ void ProcessButton_1() {
 
 }
 
+void ProcessButton_2() {
+  // just a simple way to toggle a LED on/off. Much better ways to do this
+  Serial.println("Button 1 press");
 
+  //     server.send(200, "text/plain", ""); //Send web page
+
+  if (!entry_open ) {
+
+    entry_open = true;
+    entry_open_time = millis();
+
+    //    Serial.println("Cars number reset.");/
+    server.send(200, "text/plain", "true"); //Send web page
+
+  }
+  else {
+    server.send(200, "text/plain", "false"); //Send web page
+  }
+
+}
+void ProcessButton_3() {
+
+  if (!exit_open ) {
+    exit_open = true;
+    exit_open_time = millis();
+
+    //    Serial.println("Cars number reset.");/
+    server.send(200, "text/plain", "true"); //Send web page
+
+  }
+  else {
+    server.send(200, "text/plain", "false"); //Send web page
+  }
+
+}
 // code to send the main web page
 // PAGE_MAIN is a large char defined in SuperMon.h
 void SendWebsite() {
